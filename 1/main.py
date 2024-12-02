d0914373da6d6aff8319400d93e04943797850cf
Connor Smith <connorhsmith@gmail.com>
2024-12-03 01:58:29 +1100

    Day one solution

---
+++ 1/main.py
@@ -0,0 +1,70 @@
+import sys
+from loguru import logger
+
+
+INPUT_FILE = "input.txt"
+
+logger.remove()
+logger.add(sys.stderr, level="DEBUG")
+
+
+def main() -> None:
+    left_list, right_list = parse_input(
+        read_input(INPUT_FILE),
+    )
+
+    # print(f"Total distance: {do_the_first_thing(left_list, right_list)}")
+    print(f"Similarity score: {do_the_second_thing(left_list, right_list)}")
+
+
+def do_the_first_thing(left: list, right: list) -> int:
+    left.sort()
+    right.sort()
+
+    total_distance = 0
+
+    for i in range(len(left)):
+
+        logger.debug(
+            f"Left: {left[i]} | Right: {right[i]} | Distance: {abs(left[i] - right[i])}"
+        )
+
+        total_distance += abs(left[i] - right[i])
+
+    return total_distance
+
+
+def do_the_second_thing(left: list, right: list) -> int:
+
+    similarity_score = 0
+
+    for l in left:
+        occurrences = right.count(l)
+        similarity = l * occurrences
+
+        logger.debug(f"Left: {l} | Occurrences: {occurrences} | Similarity: {similarity}")
+
+        similarity_score += similarity
+
+    return similarity_score
+
+
+def parse_input(input: list[str]) -> tuple[list, list]:
+    left_list = list()
+    right_list = list()
+
+    for line in input:
+        split = line.split("   ")
+        left_list.append(int(split[0]))
+        right_list.append(int(split[1]))
+
+    return left_list, right_list
+
+
+def read_input(filename: str) -> list[str]:
+    with open(filename, "r") as f:
+        return f.read().splitlines()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 1/tests/test_main.py
@@ -0,0 +1,70 @@
+import pytest
+from main import parse_input, do_the_first_thing, do_the_second_thing
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        (
+            [
+                "1   2",
+                "3   4",
+                "5   6",
+            ],
+            (
+                [1, 3, 5],
+                [2, 4, 6],
+            ),
+        ),
+        (
+            [
+                "2348   9503",
+                "89043   8343",
+                "2484   3894",
+            ],
+            (
+                [2348, 89043, 2484],
+                [9503, 8343, 3894],
+            ),
+        ),
+    ],
+)
+def test_parse_input(input, expected):
+    assert parse_input(input) == expected
+
+
+@pytest.mark.parametrize(
+    "input_left, input_right, expected",
+    [
+        (
+            [1, 3, 5],
+            [2, 4, 6],
+            3,
+        ),
+        (
+            [2348, 89043, 2484],
+            [9503, 8343, 3894],
+            86945,
+        ),
+    ],
+)
+def test_do_the_first_thing(input_left, input_right, expected):
+    assert do_the_first_thing(input_left, input_right) == expected
+
+@pytest.mark.parametrize(
+    "input_left, input_right, expected",
+    [
+        (
+            [1, 3, 5],
+            [1, 3, 6],
+            4,
+        ),
+        (
+            [10, 20, 30, 50, 60],
+            [20, 10, 40, 10, 10],
+            50,
+        ),
+    ],
+)
+def test_do_the_second_thing(input_left, input_right, expected):
+    assert do_the_second_thing(input_left, input_right) == expected
